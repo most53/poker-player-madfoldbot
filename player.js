f1308508bc65790c7f1ec70d14bf724e7f492356
most_53 <inlove53>
2016-03-26 15:30:22 +0200

    game state

--- player.js
+++ player.js
@@ -58,9 +58,7 @@ module.exports = {
                 console.log('LAME CARDS');
                 return 0;
             } else {
-                if (toNum(myself.hole_cards[1].rank) > 9 && toNum(myself.hole_cards[0].rank) > 9 ||
-                    (myself.hole_cards[1].suit == myself.hole_cards[1].suit)
-                ) {
+                if (toNum(myself.hole_cards[1].rank) > 9 && toNum(myself.hole_cards[0].rank) > 9) {
                     console.log('HIGH CARDS. DOING CALL');
                     return call;
                 }
