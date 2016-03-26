cbdf5dc58844742eb6b4e1118e38eab162b27392
most_53 <inlove53>
2016-03-26 12:42:25 +0200

    game state

--- player.js
+++ player.js
@@ -1,3 +1,5 @@
+var rank = require('./rank');
+
 module.exports = {
 
     VERSION: "Default JavaScript folding player",
@@ -13,6 +15,8 @@ module.exports = {
     strategy1: function(game_state, bet) {
         var current_bet = 0;
         var our_cards = [];
+
+
 
         for (var p in game_state.players) {
             if (typeof game_state.players[p]['hole_cards'] != 'undefined') {
@@ -34,37 +38,7 @@ module.exports = {
         return bet(current_bet);
     },
 
-
-    isHigh: function (card) {
-        return ['J', 'Q', 'K', 'A'].indexOf(card) == -1 ? false : true;
-    },
-
-    strategy1: function(game_state, bet) {
-        var current_bet = game_state.pot;
-        var our_cards = {};
-
-        for (var p in game_state.players) {
-            if (typeof game_state.players[p]['hole_cards'] != 'undefined') {
-                our_cards = game_state.players[p]['hole_cards'];
-            }
-        }
-        console.log('OUR CARDS');
-        console.log(our_cards);
-
-        console.log('COMMUNITY CARDS');
-        console.log(game_state);
-
-        if (our_cards[0]['rank'] == our_cards[1]['rank']) {
-            console.log('HAS PAIR!');
-            current_bet += 100;
-        }
-
-        this.getPairs(game_state);
-
-        return bet(current_bet);
-    },
-
-    getPairs: function(game_state) {
+    getAllCards: function (game_state) {
         var cards = [];
         var community_cards = game_state.community_cards;
 
@@ -77,9 +51,7 @@ module.exports = {
 
         cards = cards.concat(community_cards);
 
-        console.log('ALL CARDS');
-        console.log(cards);
-        var pairs = [];
+        var pairs = {};
         for (var c in cards) {
             if (typeof pairs[cards[c]['rank']] == 'undefined') {
                 pairs[cards[c]['rank']] = 1;
@@ -88,35 +60,9 @@ module.exports = {
             }
         }
 
-        console.log("ALL PAIRS");
-        console.log(pairs);
-
         return pairs;
-
-    },
-
-    shouldPlay: function ( card1, card2 ) {
-
-    if ( card1.rank === 'A' || card2.rank === 'A' ) {
-        return true;
     }
 
-    if ( ranks.indexOf(card1.rank) > -1 && ranks.indexOf(card2.rank) > -1 ) {
-        return true;
-    }
-
-    if ( card1.rank === card2.rank ) {
-        return true;
-    }
-
-    if ( card1.suit === card2.suit ) {
-        if ( isAKQ(card1) || isAKQ(card2) ) {
-            return true;
-        }
-    }
-
-    return false;
-}
 };
 
 
---
+++ rank.js
@@ -0,0 +1,97 @@
+/**
+ * Created by max on 3/26/16.
+ */
+exports.groupBy = groupBy
+exports.count = count
+exports.rank = rank
+
+function groupBy(array, key) {
+    var resultMap = {}
+
+    array.forEach(function(item) {
+        var value = item[key];
+
+        if (typeof resultMap[value] == "undefined") {
+            resultMap[value] = []
+        }
+
+        resultMap[value].push(item)
+    })
+
+    return resultMap
+}
+
+function count(map) {
+    var resultMap = {}
+
+    for (key in map) {
+        resultMap[key] = map[key].length
+    }
+
+    return resultMap;
+}
+
+function keyOf(array, needle, first_index) {
+    if (typeof first_index == "undefined")
+        first_index = 0
+
+    for (var index = first_index; index < array.length; index++) {
+        var item = array[index]
+
+        if (item == needle)
+            return key
+    }
+
+    return -1
+}
+
+function rank(cards) {
+    var byRank = groupBy(cards, 'rank')
+    var bySuit = groupBy(cards, 'suit')
+    var rankCounts = count(byRank)
+    var suitCounts = count(bySuit)
+    var result = []
+
+    var numberOfPairs = 0
+    var numberOfThrees = 0
+    var hasFlush = false
+
+    for (var rank in rankCounts) {
+        if (rankCounts[rank] == 2) {
+            numberOfPairs++
+        }
+
+        if (rankCounts[rank] == 3) {
+            numberOfThrees++
+        }
+    }
+
+    for (var suit in suitCounts) {
+        if (suitCounts[suit] == 5 ||
+            suitCounts[suit] == 6 ||
+            suitCounts[suit] == 7)
+        {
+            hasFlush = true
+        }
+    }
+
+    if (numberOfPairs > 0) {
+        if (numberOfPairs > 1) {
+            if (numberOfThrees > 0) {
+                result.push('full_house')
+            } else {
+                result.push('two_pairs')
+            }
+        } else {
+            result.push('pair')
+        }
+    } else if (numberOfThrees > 0) {
+        result.push('tree_of_a_kind')
+    }
+
+    if (hasFlush) {
+        result.push('flush')
+    }
+
+    return result
+}
