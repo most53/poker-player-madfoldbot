7a495b0bf6ebbae560a4c6ff761babe15820c8e9
most_53 <inlove53>
2016-03-26 10:48:06 +0200

    game state

--- player.js
+++ player.js
@@ -4,7 +4,7 @@ module.exports = {
   VERSION: "Default JavaScript folding player",
 
   bet_request: function(game_state, bet) {
-    console.log(game_state);
+    console.log(game_state.players);
     console.log(bet);
     bet(0);
   },
