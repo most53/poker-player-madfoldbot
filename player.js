8346e4800994cc6d410726e6bd489dce44a27a56
most_53 <inlove53>
2016-03-26 10:50:35 +0200

    game state

--- player.js
+++ player.js
@@ -4,8 +4,14 @@ module.exports = {
   VERSION: "Default JavaScript folding player",
 
   bet_request: function(game_state, bet) {
-    console.log(game_state.players);
-    console.log(bet);
+    var our_cards = [];
+    for( var p in game_state.players) {
+      if (typeof game_state.players[p]['hole_cards'] != 'undefined') {
+        our_cards = game_state.players[p]['hole_cards'];
+      }
+    }
+    console.log(our_cards);
+    //console.log(bet);
     bet(0);
   },
 
