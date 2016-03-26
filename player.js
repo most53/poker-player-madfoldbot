1c9c998b351fac51efc8b1e4f3d216d894746967
most_53 <inlove53>
2016-03-26 11:40:03 +0200

    game state

--- player.js
+++ player.js
@@ -3,6 +3,14 @@ module.exports = {
     VERSION: "Default JavaScript folding player",
 
     bet_request: function (game_state, bet) {
+        return this.strategy1(game_state, bet);
+    },
+
+    showdown: function (game_state) {
+
+    },
+
+    strategy1: function(game_state, bet) {
         var current_bet = 0;
         var our_cards = [];
 
@@ -19,17 +27,13 @@ module.exports = {
 
         if (our_cards[0]['rank'] == our_cards[1]['rank']) {
             console.log('HAS PAIR!');
-            current_bet = 300;
+            current_bet += 300;
         }
 
         console.log('OUR BET:' + current_bet);
-        bet(current_bet);
+        return bet(current_bet);
     },
 
-    showdown: function (game_state) {
-
-    }
-    /*
 
     isHigh: function (card) {
         return ['J', 'Q', 'K', 'A'].indexOf(card) == -1 ? false : true;
@@ -62,7 +66,6 @@ module.exports = {
         var cards = [];
         var community_cards = game_state.community_cards;
 
-
         for (var p in game_state.players) {
             if (typeof game_state.players[p]['hole_cards'] != 'undefined') {
                 cards = game_state.players[p]['hole_cards'];
@@ -73,10 +76,10 @@ module.exports = {
 
         console.log("PAIRS");
         console.log(cards);
-        console.log(game_state.community_cards);
+       // console.log(game_state.community_cards);
 
 
-    }*/
+    }
 };
 
 
